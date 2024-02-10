6ad313e7ba7887a6d70a6091038955282ae26f74
“sasuke_kh2” <amooromer2004@gmail.com>
2024-02-10 21:41:03 +0200

    up

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,8 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         result = result
     finally:
         print("Inside result: {}".format(result))
         return result
-    
