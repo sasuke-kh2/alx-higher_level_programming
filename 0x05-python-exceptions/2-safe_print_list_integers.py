3c6c8ef5a10fb80237ba513ee6bf757ac217bdaa
“sasuke_kh2” <amooromer2004@gmail.com>
2024-02-10 15:59:45 +0200

    update

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for pr in my_list:
+        try:
+            if pr == my_list[x]:
+                break
+        except IndexError:
+            x = x
+        try:
+            print("{:d}".format(pr), end="")
+            counter = counter + 1
+        except IndexError:
+            break
+        except TypeError:
+            continue
+        except ValueError:
+            continue
+    print()
+    return counter
