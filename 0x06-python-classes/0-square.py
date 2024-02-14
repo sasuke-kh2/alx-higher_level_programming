ecb1cbbbfc111c0804bea035b004d6e271aaef49
“sasuke_kh2” <amooromer2004@gmail.com>
2024-02-14 14:06:14 +0200

    up

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Square module.
+
+This module contains an empty class named Square
+"""
+
+
+class Square:
+    """Empty class Square.
+
+    This class does not have any attributes or methods yet.
+    """
+    pass
