66c61a53b04c70b7539a5ca199fe4a401984c5ff
“sasuke_kh2” <amooromer2004@gmail.com>
2024-02-14 22:08:13 +0200

    upd

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,11 +7,11 @@ The Square module contains class called Square
 
 class Square:
     """Square class
-    
+
     this class contains Private instance attribute: size.
     """
     def __init__(self, __size):
         """Private instance attribute
-        
+
         """
         self.__size = __size
