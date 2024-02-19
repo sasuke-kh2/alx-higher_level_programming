449ec7d38d7b4116d3ec576965f076fad6d9fd21
“sasuke_kh2” <amooromer2004@gmail.com>
2024-02-19 23:46:20 +0200

    up

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""this moudle containes Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle with width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    
+    def area(self):
+        """area methode"""
+        return self.__height * self.__width
+    
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        return (self.__height + self.__width) * 2
