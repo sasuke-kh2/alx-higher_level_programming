e0feb425ac753a534091e2457fa2f4cdfd395ca3
sasuke-kh2 <amooromer2004@gmail.com>
2024-02-29 16:30:20 +0200

    d

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this moudule includes
+    the bellow function"""
+
+
+def inherits_from(obj, a_class):
+    """checks if the obj is an instans of
+    a_class"""
+    return issubclass(obj, a_class)
