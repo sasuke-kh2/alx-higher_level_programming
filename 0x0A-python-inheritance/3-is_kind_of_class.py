b7d689791fed576a55bb756e2692b5dd5502e0ae
sasuke-kh2 <amooromer2004@gmail.com>
2024-02-29 16:28:04 +0200

    u

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this moudule includes
+    the bellow function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if the obj is an instans of
+    a_class"""
+    return isinstance(obj, a_class)
