102d2364576e8ded7a867ac0652c2a0b96af1e55
sasuke-kh2 <amooromer2004@gmail.com>
2024-02-29 16:25:40 +0200

    f

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""this moudule includes
+    the bellow function"""
+
+
 def is_same_class(obj, a_class):
+    """checks if the obj is an instans of
+    a_class"""
     return type(obj) is a_class
