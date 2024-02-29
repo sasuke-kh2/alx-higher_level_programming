53c4b8be71b94118b22a36078b7759f3f73745c1
sasuke-kh2 <amooromer2004@gmail.com>
2024-02-29 16:42:56 +0200

    d

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this moudule includes
+    the bellow class"""
+
+
+class BaseGeometry:
+    """not an empty class"""
+
+
+    def area(self):
+        """Public instance metho"""
+        raise("area() is not implemented")
