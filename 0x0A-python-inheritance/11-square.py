334e4f22c93e7f0940391e353bc52f578481b5af
sasuke-kh2 <amooromer2004@gmail.com>
2024-03-02 15:34:29 +0200

    gg

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -50,7 +50,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return a string representation of the square."""
-        return f"[Rectangle] {self.__size}/{self.__size}"
+        return f"[square] {self.__size}/{self.__size}"
 
     def area(self):
         """return a square area"""
